0181f4549e6eb7793e964397d607a1b88d59907a
Andrew <andrew@truepill.com>
2020-06-07 10:11:59 +0100

    Support none-innerText options

--- README.md
+++ README.md
@@ -2,16 +2,32 @@
 
 `dollar-sign` takes an input stream, parses it as HTML, then returns the inner text of any elements that match a supplied selector.
 
-I built it because I was doing a crossword and thought an answer might be a four-letter country ending in 'I'. (It wasn't.) I'd found [a list of countries](curl https://www.britannica.com/topic/list-of-countries-1993160) but it wasn't sortable by length or last letter so it didn't help much.
+I built it because I was doing a crossword and the clue "Mark fighter for country (4)" came up. The answer presumably was something that means 'mark', followed by a fighter, which makes a four-letter country, and since I already had 12 across, I knew it ended in "I". I'd found [a list of countries](curl https://www.britannica.com/topic/list-of-countries-1993160) but it wasn't sortable by length or last letter so it didn't help much.
 
 With this tool, I can do this:
 
 ``` sh
-
 curl https://www.britannica.com/topic/list-of-countries-1993160 \
 	| dollar-sign ".topic-list li" \
 	| egrep -i "^[a-z]{3}i$";
-
 ```
 
-It reports the only two countries that match my criteria are Fiji and Mali.
+It reports the only four-letter countries ending in I are Fiji and Mali. "M" means "Mark" and "ALI" is a fighter, so that was the answer.
+
+I expect this tool has other uses.
+
+## Options
+
+If it isn't the inner text you want, you can pick out attributes:
+
+```sh
+curl http://example.com \
+	| dollar-sign -a class "li > a"
+```
+
+Or you can pick out the tag name:
+
+```sh
+curl http://example.com \
+	| dollar-sign -t ".some-class"
+```
--- index.js
+++ index.js
@@ -1,21 +1,61 @@
 #!/usr/bin/env node
 
-const cheerio = require('cheerio');
+const $ = require('cheerio');
 
-if (process.argv.length != 3) {
-	console.error('Usage:');
-	console.error('  curl [URL] | dollar-sign [SELECTOR]');
-	console.error('  curl http://example.com | dollar-sign ".main li"');
-	process.exit(1);
-}
-const selector = process.argv[2];
+const options = parseArgs(process.argv.slice(2));
 
 let buffers = [];
 process.stdin.on('data', b => buffers.push(b));
 process.stdin.on('end', () => {
-	const html = Buffer.concat(buffers).toString(),
-		$ = cheerio(html),
-		els = $.find(selector);
-	for (const el of els.toArray())
-		console.log(cheerio(el).text());
+	const html = Buffer.concat(buffers).toString();
+	parseHtml(html, options);
 });
+
+function parseHtml(html, { stringifier, selector }) {
+	const els = $(html).find(selector);
+	for (const el of els.toArray())
+		console.log(stringifier(el));
+}
+
+function parseArgs(args) {
+	if (args.length < 1) dieWithUsage();
+	const selector = args.pop();
+	let stringifier = el => $(el).text();
+	while (args.length) {
+		switch (args.shift()) {
+			case '-a':
+			case '--attr':
+			case '--attribute':
+				const attr = args.shift();
+				stringifier = el => $(el).attr(attr);
+				break;
+			case '-t':
+			case '--tag':
+				stringifier = el => el.tagName;
+				break;
+			default:
+				dieWithUsage();
+		}
+	}
+	return { stringifier, selector };
+}
+
+function dieWithUsage() {
+	console.error('');
+	console.error('Usage:');
+	console.error('  curl [URL] | dollar-sign [OPTIONS] [SELECTOR]');
+	console.error('  curl http://example.com | dollar-sign ".main li"');
+	console.error('  curl http://example.com | dollar-sign -t ".main li"');
+	console.error('  curl http://example.com | dollar-sign -a data-value ".main li"');
+	console.error('');
+	console.error('Allowed options:');
+	console.error('');
+	console.error('  -a ATTR             Instead of the inner text,');
+	console.error('  --attr ATTR         return the value of the attribute ATTR.');
+	console.error('  --attribute ATTR    ');
+	console.error('');
+	console.error('  -t                  Instead of the inner text,');
+	console.error('  --tag               return the HTML tag.');
+	console.error('');
+	process.exit(1);
+}
